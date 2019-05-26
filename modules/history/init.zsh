5a36a7975d3ff6d2d9e872d9683ca2b989284132
John O'Shea <john.m.oshea@gmail.com>
2019-05-26 15:42:41 -0600

    Don't add common, low-information commands to history

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -10,9 +10,10 @@
 # Variables
 #
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
-HISTSIZE=100000                   # The maximum number of events to save in the internal history.
-SAVEHIST=100000                   # The maximum number of events to save in the history file.
+HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"      # The path to the history file.
+HISTSIZE=100000                             # The maximum number of events to save in the internal history.
+SAVEHIST=100000                             # The maximum number of events to save in the history file.
+HISTORY_IGNORE="(ls|cd|pwd|exit|cd ..|fg)"  # Ignore common, low-information, commands
 
 #
 # Options
